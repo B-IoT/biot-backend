1c784f9fdb0a7c5e79f1a1978676f89ee37fed0c
Andrea Veneziano <andrea.veneziano@epfl.ch>
2020-10-12 10:05:19 +0200

    Add input validation for BaseItem (battery) and defaults for status

--- backend/database/schemas.py
+++ backend/database/schemas.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 from enum import Enum
 
 
@@ -9,10 +9,19 @@ class StatusEnum(str, Enum):
 
 class BaseItem(BaseModel):
     type: str
-    status: StatusEnum
+    status: StatusEnum = StatusEnum.available
     battery: int
     latitude: float
     longitude: float
+
+    @validator("battery")
+    def battery_must_be_in_range(cls, v):
+        if v < 0 or v > 100:
+            raise ValueError("The battery level must be between 0 and 100")
+
+        return v
+
+    # TODO: define type as an Enum
 
 
 class Item(BaseItem):
